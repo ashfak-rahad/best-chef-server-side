448e2d0beab75774718ea37e1e609311f32fe9ed
ashfak-rahad <ashfakur@gmail.com>
2023-05-03 01:28:37 +0600

    some changes gitIgnorfile

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require ('express')
-const app =express();
-const port = 4003;
+const express = require('express')
 
+const app = express();
+const port = process.env.port || 4000;
+const chef = require('./chef.json');
 
-app.get ('/',(req,res)=>{
-    res.send('hello world')
+app.get('/chef', (req, res) => {
+    res.send(chef);
 })
-app.listen(port,()=>{
+app.listen(port, () => {
+    console.log(`running on port:${port}`);
 
 })
