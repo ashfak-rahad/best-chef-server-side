157b432a30252359feaed7d0d643795ec4f01b48
ashfak-rahad <ashfakur@gmail.com>
2023-05-03 01:58:55 +0600

    git ignore

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const cors = require ('cors')
 
 const app = express();
 const port = process.env.port || 4000;
 const chef = require('./chef.json');
-
+app.use(cors())
 app.get('/chef', (req, res) => {
     res.send(chef);
 })
